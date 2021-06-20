c9694916da6c82e2ac05a9b5692fce057953b2b9
Lashchinskiy Artyom <lashchinskiy.artyom@gmail.com>
2021-06-20 14:21:06 +0300

    Cryptonomicon 4 - Vue Practice - Add saving to localStorage

--- src/App.vue
+++ src/App.vue
@@ -195,7 +195,35 @@ export default {
     }
   },
 
+  created() {
+    const tickersData = localStorage.getItem("crypto-list");
+
+    if (tickersData) {
+      this.tickers = JSON.parse(tickersData);
+      this.tickers.forEach(ticker => {
+        this.subscribeToUpdates(ticker.name);
+      });
+    }
+  },
+
   methods: {
+    subscribeToUpdates(tickerName) {
+      setInterval(async () => {
+        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${tickerName}&tsyms=USD&api_key=ae31660f9db182aec72ab510fe70a19eef6fac2584e3fb22855bbddb42d5a959`);
+        const data = await f.json();
+        
+        this.tickers
+            .find(t => t.name === tickerName)
+            .price = data.USD > 1 
+            ? data.USD.toFixed(2) 
+            : data.USD.toPrecision(4);
+        
+        if (this.selectedItem?.name === tickerName) {
+          this.graph.push(data.USD);
+        }
+      }, 5000);
+    },
+
     add() {
       const currentTicker = {
         name: this.ticker,
@@ -203,21 +231,11 @@ export default {
       };
 
       this.tickers.push(currentTicker);
-      setInterval(async () => {
-        const f = await fetch(`https://min-api.cryptocompare.com/data/price?fsym=${currentTicker.name}&tsyms=USD&api_key=ae31660f9db182aec72ab510fe70a19eef6fac2584e3fb22855bbddb42d5a959`);
-        const data = await f.json();
-        
-        this.tickers
-            .find(t => t.name === currentTicker.name)
-            .price = data.USD > 1 
-            ? data.USD.toFixed(2) 
-            : data.USD.toPrecision(4);
-        
-        if (this.selectedItem?.name === currentTicker.name) {
-          this.graph.push(data.USD);
-        }
-      }, 5000);
-      this.ticker = '';
+
+      localStorage.setItem("crypto-list", JSON.stringify(this.tickers));
+      this.subscribeToUpdates(currentTicker.name);
+
+      this.ticker = "";
     },
 
     handleDelete(tickerToRemove) {
